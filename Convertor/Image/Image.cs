5932c46e28f73ee5cb8ab453e261c4302064e163
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-07 17:55:36 +0200

    Add abstract class of image and Converter interface

---
+++ Convertor/Image/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Convertor.Image
+{
+    public abstract class Image
+    {
+        public abstract int Height { get; set; }
+        public abstract int Width { get; set; }
+        public abstract string Path { get; set; }
+    }
+}
---
+++ Convertor/Interfaces/IConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Convertor.Interfaces
+{
+    public interface IConverter
+    {
+        public void Convert(string path);
+    }
+}
