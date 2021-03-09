784df0f9111f34f435a73a08db21745877f902e1
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-09 23:19:36 +0200

    Add basic GIF header writing

---
+++ Convertor/GifWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Converter.ImageBase;
+using Converter.Interfaces;
+
+namespace Converter
+{
+    public class GifWriter : IImageWriter
+    {
+        public void Write(string path, Image image)
+        {
+            using BinaryWriter writer = new BinaryWriter(File.Open(path + ".gif", FileMode.Create));
+
+            writer.Write("GIF87a");
+
+            writer.Write((short)image.Header.Width);
+            writer.Write((short)image.Header.Height);
+
+            byte packedField = Convert.ToByte("11110111", 2);
+            writer.Write(packedField);
+
+            byte bgColorIndex = 0;
+            writer.Write(bgColorIndex);
+
+            byte pixelAspectRatio = 0;
+            writer.Write(pixelAspectRatio);
+
+
+        }
+    }
+}
