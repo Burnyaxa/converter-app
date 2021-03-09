41822e57505777f30cf2f63746e639e3308bea04
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-09 17:43:18 +0200

    Add Converter class

---
+++ Convertor/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Converter.Interfaces;
+
+namespace Converter
+{
+    public class Converter : IConverter
+    {
+        private IImageReader _reader;
+        private IImageWriter _writer;
+
+        public Converter(IImageReader reader, IImageWriter writer)
+        {
+            _reader = reader;
+            _writer = writer;
+        }
+
+        public Image.Image Convert(Image.Image image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
