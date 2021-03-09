4d0ff068474615b5bdaf58367213998ff589fb04
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-09 17:40:04 +0200

    Fix typo in namespaces

--- Convertor/Interfaces/IConverter.cs
+++ Convertor/Interfaces/IConverter.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Convertor.Image;
-namespace Convertor.Interfaces
+
+namespace Converter.Interfaces
 {
     public interface IConverter
     {
--- Convertor/Interfaces/IImageReader.cs
+++ Convertor/Interfaces/IImageReader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Convertor.Interfaces
+namespace Converter.Interfaces
 {
     public interface IImageReader
     {
