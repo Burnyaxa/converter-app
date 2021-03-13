98dd2a53c3bf5665ef98e1578df36a61b0ee3579
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-14 01:45:10 +0200

    Add UI and KeyHandling

---
+++ Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Converter;
+
+namespace ConsoleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var value = KeyHandler.GetValues(args);
+                ConversionFacade.InitiateConversion(value.source, value.destination, value.format);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
