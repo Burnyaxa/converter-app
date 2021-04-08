60ffa12c5883e9259a0395cb56b78e6165880f21
Artem Supryhan <suprigan.artem1@gmail.com>
2021-04-08 17:40:15 +0300

    Add camera position provide and renderer class

---
+++ Convertor/Renderers/Renderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Converter.Interfaces;
+
+namespace Converter.Renderers
+{
+    public class Renderer
+    {
+        private readonly ICameraPositionProvider _provider;
+
+        public Renderer(ICameraPositionProvider provider)
+        {
+            _provider = provider;
+        }
+
+
+    }
+}
