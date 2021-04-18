e64ca8f06ecb3b5879902cc4e938508daa93e802
Artem Supryhan <suprigan.artem1@gmail.com>
2021-04-18 20:54:29 +0300

    Add forgotten files XD

---
+++ Convertor/Interfaces/IVectorConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Converter.ImageBase;
+
+namespace Converter.Interfaces
+{
+    public interface IVectorConverter
+    {
+        public Color[,] ConvertFromVectorToColors(Vector3[,] vectors);
+    }
+}
